66137c41010235db583565ca1e9d5e838b044fdb
JiaoZhengXiang <jiaozhengxiang@foxmail.com>
2016-06-27 12:14:11 +0800

    Version1.0.1

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Able to set the minimun and maximum millseconds.
 
 ## gradle, latest version:
 ```java
-   compile 'com.jzxiang.pickerview:TimePickerDialog:1.0.0'
+   compile 'com.jzxiang.pickerview:TimePickerDialog:1.0.1'
 ```
 ## Demo picture
 ![](https://github.com/JZXiang/PickerView/raw/master/preview/timepickerdialog_demo.gif)
--- TimePickerDialog/src/main/java/com/jzxiang/pickerview/config/DefaultConfig.java
+++ TimePickerDialog/src/main/java/com/jzxiang/pickerview/config/DefaultConfig.java
@@ -13,14 +13,14 @@ public class DefaultConfig {
     public static final int TV_SELECTOR_COLOR = 0XFF404040;
     public static final int TV_SIZE = 12;
     public static final boolean CYCLIC = true;
-    public static String CANCEL = "Cancel";
-    public static String SURE = "Sure";
+    public static String CANCEL = "取消";
+    public static String SURE = "确定";
     public static String TITLE = "TimePicker";
-    public static String YEAR = "Year";
-    public static String MONTH = "Month";
-    public static String DAY = "Day";
-    public static String HOUR = "Hour";
-    public static String MINUTE = "Minute";
+    public static String YEAR = "年";
+    public static String MONTH = "月";
+    public static String DAY = "日";
+    public static String HOUR = "时";
+    public static String MINUTE = "分";
 
 
 }
